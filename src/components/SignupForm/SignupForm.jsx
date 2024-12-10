8b97bb4b4cd60cd61e07d34bdfa0959b37ed119e
deijahmonay <deijah@shack15.com>
2024-12-09 19:51:15 -0800

    cret\ate signup form component with username, email and password fileds

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const SignupForm = (props) => {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '', 
+    password: '',
+    passwordConf: '',
+  });
+
+  const updateMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    updateMessage('');
+    console.log(formData); 
+  };
+
+  const { username, email, password, passwordConf } = formData;
+
+  const isFormInvalid = () => {
+    return !(username && email && password && password === passwordConf);
+  };
+
+  return (
+    <main>
+      <h1>Sign Up</h1>
+      <p>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            name="username"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            name="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm"
+            value={passwordConf}
+            name="passwordConf"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button disabled={isFormInvalid()}>Sign Up</button>
+          <Link to="/">
+            <button type="button">Cancel</button>
+          </Link>
+        </div>
+      </form>
+    </main>
+  );
+};
+
+export default SignupForm
