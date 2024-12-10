9eb2386e7c57605910f1478a5a512da0e5f85f09
deijahmonay <deijah@shack15.com>
2024-12-09 20:01:30 -0800

    import signupform + add signup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// src/App.jsx
-
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Dashboard from './components/Dashboard/Dashboard';
 import Landing from './components/Landing/Landing';
+import SignupForm from './components/SignupForm/SignupForm';
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -18,6 +17,7 @@ const App = () => {
         ) : (
           <Route path='/' element={<Landing />}/>
         )}
+        <Route path='/signup' element={<SignupForm />} />
       </Routes>
     </>
   )
