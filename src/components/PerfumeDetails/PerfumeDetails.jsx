c14958ae8374fa5a300bc692c0faf76f23862eaf
deijahmonay <deijah@shack15.com>
2024-12-10 02:13:34 -0800

    import perfumeservice

--- src/components/PerfumeDetails/PerfumeDetails.jsx
+++ src/components/PerfumeDetails/PerfumeDetails.jsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom"
+import * as perfumeService from '../../services/perfumeService'
 
 const PerfumeDetails = (props) => {
   const {perfumeId} = useParams()
-  
+
   return <main>Perfume Details</main>
 }
 
