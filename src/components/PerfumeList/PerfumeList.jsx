aaaff78ae697bbd5dd52c254638ddfdc5f75963d
deijahmonay <deijah@shack15.com>
2024-12-10 01:41:33 -0800

    updated component for rendering all perfume details (name,brand,author)

--- src/components/PerfumeList/PerfumeList.jsx
+++ src/components/PerfumeList/PerfumeList.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+
 const PerfumeList = (props) => {
   return (
     <main>
-      {props.perfumes.map((perfume) => {
-        return  <p key={perfume._id}>{perfume.name}</p>
-      })}
+      {props.perfumes.map((perfume) => (
+        <Link key={perfume._id} to={`/perfumes/${perfume._id}`}>
+          <article>
+            <header>
+              <h2>{perfume.name}</h2>
+              <p>
+                {perfume.author?.username} posted on{" "}
+                {new Date(perfume.createdAt).toLocaleDateString()}
+              </p>
+            </header>
+            <p>{perfume.brand}</p> 
+          </article>
+        </Link>
+      ))}
     </main>
   )
 }
